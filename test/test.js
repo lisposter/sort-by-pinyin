9ca4fba5b1179b52fd70b6322e1e9cd21f7a6c34
Leigh Zhu <i@zhuli.me>
2015-04-11 11:26:56 +0800

    fix test

--- test/test.js
+++ test/test.js
@@ -5,6 +5,6 @@ var sort = require('..');
 
 describe('sort:', function() {
   it('should sort the Chinese words by Pinyin', function() {
-    sort(['上海', '北京', '杭州', '长沙']).should.be.exactly([ '北京', '长沙', '杭州', '上海' ]);
+    sort(['上海', '北京', '杭州', '长沙']).join().should.be.equal(['北京', '长沙', '杭州', '上海'].join());
   });
 });
